5a7abf035da706a9c7cad711948293072e805f2b
Aelpecyem <aelpecyem@gmail.com>
2019-10-31 19:17:33 +0100

    improved the lit rainbow particles

--- src/main/java/de/aelpecyem/elementaristics/client/particle/mode/ParticleModeSpectrum.java
+++ src/main/java/de/aelpecyem/elementaristics/client/particle/mode/ParticleModeSpectrum.java
@@ -2,7 +2,6 @@ package de.aelpecyem.elementaristics.client.particle.mode;
 
 import de.aelpecyem.elementaristics.client.particle.GlowParticle;
 import de.aelpecyem.elementaristics.reg.ModParticles;
-import de.aelpecyem.elementaristics.util.ColorUtil;
 import net.minecraft.client.particle.IParticleRenderType;
 
 import java.awt.*;
@@ -18,18 +17,8 @@ public class ParticleModeSpectrum extends ParticleMode {
 
     @Override
     public void tick(GlowParticle particle) {
-        int tick = particle.getAge() % 300;
-        int tickParts = tick % 100;
-        float blend = tickParts / 100F;//(0.5F - Math.abs(0.5F - tickParts / 100F)) * 2F;//tickParts - 200;
-        Color color;
-        System.out.println(blend);
-        if (tick <= 100) {
-            color = ColorUtil.blend(Color.BLUE, Color.RED, blend, 1 - blend);
-        } else if (tick <= 200) {
-            color = ColorUtil.blend(Color.RED, Color.GREEN, blend, 1 - blend);
-        } else {
-            color = ColorUtil.blend(Color.GREEN, Color.BLUE, blend, 1 - blend);
-        }
+        float process = (particle.getAge() % 100) / 100F;
+        Color color = Color.getHSBColor(process, 1, 1);
         particle.setColor(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F);
     }
 
